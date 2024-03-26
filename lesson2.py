9570a5e77176d1ed51670cd8eba2fdeecad5aff9
richardmarani <maranirichard@gmail.com>
2024-03-26 16:46:00 +0300

    first commit

---
+++ lesson2.py
@@ -0,0 +1,44 @@
+# Create an empty list called my_list
+my_list = []
+
+# Append the following elements to my_list: 10, 20, 30, 40
+my_list.append([10, 20, 30, 40])
+print(my_list)
+# Insert the value 15 at the second position in the list
+my_list.insert (2,15)
+print(my_list)
+# Extend my_list with another list: [50, 60, 70]
+my_list.extend([50, 60, 70])
+print(my_list)
+# Remove the last element from my_list
+my_list.pop()
+print(my_list)
+x=([10,20,30,40,15,50,60,70])
+print(x)
+# Sort my_list in ascending order
+x.sort()
+for element in x:
+    print(element)
+# Find and print the index of the value 30 in my_list
+# Find and print the index of the value 30 in my_list
+index_30 = my_list.index(30)
+print("Index of 30 in my_list:", index_30)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
